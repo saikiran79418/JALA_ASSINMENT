d01c9005e9ef3fc4a5edf8696614695c2ee699c0
saikiran79418 <saikiran790151@gmail.com>
2025-08-13 18:29:19 +0530

    Initial third commit  project upload

--- 1.1.py
+++ 1.1.py
@@ -1,9 +1,9 @@
 #1. Write a program to print your name.
-a=input("enter your name:")
+a=input("enter your name")
 print(a)
-
-
 #output
-#enter your name:saikiran
+#C:\internship_python_assinment\.venv\Scripts\python.exe C:\internship_python_assinment\1.1.py
+#enter your namesaikiran
 #saikiran
+
 #Process finished with exit code 0
